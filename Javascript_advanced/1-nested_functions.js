8b2e5e9fbec5c8ca396a3f8d5c582eca3640f30e
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-03-15 18:15:24 +0300

    task completed

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,20 @@
+const globalVariable = "Welcome";
+
+function outer() {
+  alert(globalVariable);
+  const course = "Holberton";
+  const exclamation = "!";
+
+  function inner() {
+    alert(`${globalVariable} ${course}`);
+
+    function inception() {
+      alert(`${globalVariable} ${course}${exclamation}`);
+    }
+    inception();
+  }
+
+  inner();
+}
+
+outer();
---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,31 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+      let isprime = false;
+      for (let j = 2; j <= i; j++) {
+        if (i % j == 0) {
+          isprime = false;
+          break;
+        }
+      }
+  
+      if (isprime) {
+        ++count;
+      }
+    }
+  
+    return count;
+  }
+  
+  function loop() {
+    for (let k = 0; k < 100; ++k) {
+      countPrimeNumbers();
+    }
+  }
+  
+  let start = performance.now();
+  setTimeout(loop(), 0);
+  let end = performance.now();
+  
+  let final = end - start;
+  console.log(`Execution time of calculating prime numbers 100 times was ${final} milliseconds.`);
